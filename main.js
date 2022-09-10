1814803d0a1d46f6944254886641805a676a8992
MaheshRath08 <maheshrath.2002@gmail.com>
2022-09-10 16:39:37 +0530

    initial dev phase

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = document.getElementById("book-name")
+let author = document.getElementById("author")
+let bookId = document.getElementById("id")
+let submitBtn = document.getElementById("submit")
+
+let display = document.querySelector("display")
+let noti = document.querySelector("noti")
